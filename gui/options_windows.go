4a2c94ee29af480403752e1f6318231ea2982ad9
webdunesurfer <vkh@gmx.at>
2026-02-11 12:15:14 +0100

    Fix: Use absolute path in LOCALAPPDATA for webview data directory

--- gui/options_windows.go
+++ gui/options_windows.go
@@ -3,12 +3,20 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 func getWindowsOptions() *windows.Options {
+	// Use %LOCALAPPDATA%/SloPN for webview data to ensure write permissions
+	// and avoid issues when installed in Program Files.
+	localAppData := os.Getenv("LOCALAPPDATA")
+	userDataPath := filepath.Join(localAppData, "SloPN", "webview")
+
 	return &windows.Options{
-		WebviewUserDataPath:  "SloPN", // Correct field name for data folder
-		WebviewGpuIsDisabled: true,    // Correct field name to disable GPU
+		WebviewUserDataPath:  userDataPath,
+		WebviewGpuIsDisabled: true, // Best compatibility for old Windows 10
 	}
 }
